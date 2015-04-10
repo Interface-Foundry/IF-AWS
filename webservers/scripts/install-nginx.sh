579e1987ef0e0fa081e85d99a5194184a0372394
Pierig Le Saux <lesaux@pythian.com>
2015-04-10 17:35:50 -0400

    adding simple http to https rewrite as default nginx vhost

--- webservers/scripts/install-nginx.sh
+++ webservers/scripts/install-nginx.sh
@@ -1,2 +1,14 @@
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install nginx -y
+sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.orig
+
+cat <<'EOF' >> default
+server {
+       listen         80;
+       server_name    kipapp.co;
+       return         301 https://$server_name$request_uri;
+}
+EOF
+
+sudo mv /home/ubuntu/default /etc/nginx/sites-available/default
+sudo service nginx restart
