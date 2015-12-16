07aecfc5e2190d5233ae4fe311f2709549fcfc30
Peter Brandt <peter.m.brandt@gmail.com>
2015-12-16 18:24:15 -0500

    mongod devops

---
+++ devops/database/install-mongodb.sh
@@ -0,0 +1,9 @@
+sudo mkdir -p /data/journal
+sudo mkdir -p /logs
+sudo mkdir -p /journal
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo chown -R ubuntu:ubuntu /data /journal /log
+touch /log/mongod.log
